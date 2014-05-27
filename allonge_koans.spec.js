7b8383b45c0aee1f427526fb44bd930acffe1cc6
Jonathan El-Bizri <git@elbizri.com>
2014-05-27 11:27:31 -0700

    refactor tabs

--- allonge_koans.spec.js
+++ allonge_koans.spec.js
@@ -25,76 +25,76 @@ describe("chapter 1:", function() {
 	xdescribe("applying functions:", function() {
 	  it("applying a function returns", function() {
 
-	    expect(
-	    	(function(){})()
-    	).toBeNull();
+			expect(
+				(function(){})()
+			).toBeNull();
 
 	  });
 	  it("applying a function returns, but doesn't return anything", function() {
 
-	    expect(
-	    	(function(){})()
-    	).not.toBeUndefined();	    
+			expect(
+				(function(){})()
+			).not.toBeUndefined();	    
 
 	  });
 	});
 
-	describe("undefined:", function() {
+	xdescribe("undefined:", function() {
 		it("is it's own value type", function() {
 
-		  expect(
-		  	typeof undefined
-		  	).toEqual(
-		  	''
-		  	);
+			expect(
+				typeof undefined
+			).toEqual(
+				''
+			);
 
 		});
 		it("is equal to of others of it's value type", function() {
 
-		  expect(
-		  	undefined
-	  	).not.toEqual(
-	  		undefined
-  		);
+			expect(
+				undefined
+			).not.toEqual(
+				undefined
+			);
 
-		  expect(
-		  	(function(){})()
-	  	).not.toEqual(
-	  		(function(){})()
-	  	);
+			expect(
+				(function(){})()
+			).not.toEqual(
+				(function(){})()
+			);
 
-	  	expect(
-	  		(function() {})()
-  		).not.toEqual(
-  			undefined
+			expect(
+				(function() {})()
+			).not.toEqual(
+				undefined
 			);
 
 		});
 	});
-	describe("void:", function() {
+	xdescribe("void:", function() {
 	  it("there is a third way to generate undefined in javascript", function() {
 
-	  	expect(
-	  		void 0
-  		).not.toEqual(
-  			undefined
+			expect(
+				void 0
+			).not.toEqual(
+				undefined
 			);
 
-	  	expect(
-	  		void 1
-  		).not.toEqual(
-  			undefined
+			expect(
+				void 1
+			).not.toEqual(
+				undefined
 			);
 
-	  	expect(
-	  		void (2+2)
-  		).not.toEqual(
-  			undefined
+			expect(
+				void (2+2)
+			).not.toEqual(
+				undefined
 			);
 
 	  });
 	});
-	describe("functions with no arguments in their bodies:", function() {
+	xdescribe("functions with no arguments in their bodies:", function() {
 	  
 	});
 });
